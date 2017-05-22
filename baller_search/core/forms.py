1148577cb43cabf57b70458ef5db76281da18a35
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-05-22 22:08:56 +0200

    Tweak visuals

--- baller_search/core/forms.py
+++ baller_search/core/forms.py
@@ -1,10 +1,19 @@
-from haystack.forms import SearchForm, FacetedSearchForm
+from haystack.forms import SearchForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
+class PostForm(SearchForm):
 
-class PostForm(FacetedSearchForm):
-
-    pass
-
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+            'Make a query! Find baller posts!',
+            'q',
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='button white')
+            )
+        )
+        super(PostForm, self).__init__(*args, **kwargs)
