445baa41328e2bb4ff6b6b5604fc4cc948ced83b
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-05-23 22:42:49 +0200

    Add admin pages

--- baller_search/core/admin.py
+++ baller_search/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, NerdBaller
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'nerdballer', 'score', 'pub_date')
+    ordering = ('-score',)
+    search_fields = ('title', 'nerdballer__username', 'nerdballer__known_as')
+
+
+class NerdBallerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'known_as')
+    ordering = ("username", )
+    search_fields = ('username', 'known_as')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(NerdBaller, NerdBallerAdmin)
