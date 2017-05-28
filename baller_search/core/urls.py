a7917cf0ebfa5bc0734cf6ebeba431f360d009ff
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-05-28 20:55:22 +0200

    Split out results to seperate page

--- baller_search/core/urls.py
+++ baller_search/core/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.MySearchView.as_view(), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^search/$', views.MySearchView.as_view(template_name='pages/search.html'), name='search'),
 ]
