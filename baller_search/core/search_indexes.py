f3ad121f6d290f21b7dc30f8d26fb5908338c2c8
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-05-28 20:08:24 +0200

    Add blacklisting of certain subs

--- baller_search/core/search_indexes.py
+++ baller_search/core/search_indexes.py
@@ -1,3 +1,6 @@
+from os import path
+
+from django.conf import settings
 from django.utils import timezone
 
 from haystack import indexes
@@ -13,6 +16,30 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Post
 
+    def get_blacklisted_subs(self):
+        """Return a list of blacklisted subs."""
+        blacklist_location = path.join(str(settings.ROOT_DIR), "blacklisted_subs.txt")
+
+        if not path.exists(blacklist_location):
+            return []
+        else:
+            return self._read_blacklist_file(blacklist_location)
+
+    def _read_blacklist_file(self, blacklist_location):
+        """Read and parse blacklist file. Return blacklisted subs."""
+        blacklisted_subs = []
+
+        with open(blacklist_location) as infile:
+            for line in infile.readlines():
+                if not line.startswith("#") and len(line.strip()):
+                    blacklisted_subs.append(line.strip())
+
+        return blacklisted_subs
+
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(pub_date__lte=timezone.now())
+        blacklisted_subs = self.get_blacklisted_subs()
+
+        return self.get_model().objects.exclude(
+            subreddit__in=blacklisted_subs,
+        )
